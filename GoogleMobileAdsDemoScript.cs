1745ab5fe275cfda7627fc44d2f818dc4fe65bda
smartyGirlz <35099078+smartyGirlz@users.noreply.github.com>
2019-01-05 21:22:58 +0530

    Create GoogleMobileAdsDemoScript.cs
    admob unity Banner and Interstial

---
+++ GoogleMobileAdsDemoScript.cs
@@ -0,0 +1,328 @@
+using System;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+// Example script showing how to invoke the Google Mobile Ads Unity plugin.
+public class GoogleMobileAdsDemoScript : MonoBehaviour
+{
+    public BannerView bannerView;
+    public InterstitialAd interstitial;
+    public RewardBasedVideoAd rewardBasedVideo;
+    private float deltaTime = 0.0f;
+    private static string outputMessage = string.Empty;
+    public bool giveReward = false;
+    public static string OutputMessage
+    {
+        set { outputMessage = value; }
+    }
+
+    public void Start()
+    {
+
+#if UNITY_ANDROID
+        string appId = "ca-app-pub-3940256099942544~3347511713";
+#elif UNITY_IPHONE
+        string appId = "ca-app-pub-3940256099942544~1458002511";
+#else
+        string appId = "unexpected_platform";
+#endif
+
+        MobileAds.SetiOSAppPauseOnBackground(true);
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);
+
+        this.RequestBanner();
+        this.RequestInterstitial();
+        this.RequestRewardBasedVideo();
+    }
+
+   /* public void Update()
+    {
+        // Calculate simple moving average for time to render screen. 0.1 factor used as smoothing
+        // value.
+        this.deltaTime += (Time.deltaTime - this.deltaTime) * 0.1f;
+    }*/
+
+ 
+
+    // Returns an ad request with custom ad targeting.
+    public AdRequest CreateAdRequest()
+    {
+        return new AdRequest.Builder()
+            .AddTestDevice(AdRequest.TestDeviceSimulator)
+            .AddTestDevice("0123456789ABCDEF0123456789ABCDEF")
+            .AddKeyword("game")
+            .SetGender(Gender.Male)
+            .SetBirthday(new DateTime(1985, 1, 1))
+            .TagForChildDirectedTreatment(false)
+            .AddExtra("color_bg", "9B30FF")
+            .Build();
+    }
+
+
+
+
+
+    public  void RequestBanner()
+    {
+        // These ad units are configured to always serve test ads.
+#if UNITY_EDITOR
+        string adUnitId = "unused";
+#elif UNITY_ANDROID
+        string adUnitId = "ca-app-pub-6552333808021309/9056897756";//original AD UNIT ID HE
+#elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Clean up banner ad before creating a new one.
+        if (this.bannerView != null)
+        {
+            this.bannerView.Destroy();
+        }
+
+        // Create a 320x50 banner at the top of the screen.
+        this.bannerView = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.TopLeft);
+
+        // Register for ad events.
+        this.bannerView.OnAdLoaded += this.HandleAdLoaded;
+        this.bannerView.OnAdFailedToLoad += this.HandleAdFailedToLoad;
+        this.bannerView.OnAdOpening += this.HandleAdOpened;
+        this.bannerView.OnAdClosed += this.HandleAdClosed;
+        this.bannerView.OnAdLeavingApplication += this.HandleAdLeftApplication;
+
+        // Load a banner ad.
+        this.bannerView.LoadAd(this.CreateAdRequest());
+    }
+
+
+
+
+
+
+
+
+
+    public void RequestInterstitial()
+    {
+        // These ad units are configured to always serve test ads.
+#if UNITY_EDITOR
+        string adUnitId = "unused";
+#elif UNITY_ANDROID
+        string adUnitId = "ca-app-pub-6552333808021309/1182987605";  //asli intesrtial id he
+#elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Clean up interstitial ad before creating a new one.
+        if (this.interstitial != null)
+        {
+            this.interstitial.Destroy();
+        }
+
+        // Create an interstitial.
+        this.interstitial = new InterstitialAd(adUnitId);
+
+        // Register for ad events.
+        this.interstitial.OnAdLoaded += this.HandleInterstitialLoaded;
+        this.interstitial.OnAdFailedToLoad += this.HandleInterstitialFailedToLoad;
+        this.interstitial.OnAdOpening += this.HandleInterstitialOpened;
+        this.interstitial.OnAdClosed += this.HandleInterstitialClosed;
+        this.interstitial.OnAdLeavingApplication += this.HandleInterstitialLeftApplication;
+
+        // Load an interstitial ad.
+        this.interstitial.LoadAd(this.CreateAdRequest());
+    }
+
+
+
+
+
+
+    public void RequestRewardBasedVideo()//ye sab start me call to req,,,but not to display (to display use btt onclick)
+    {
+#if UNITY_EDITOR
+       string adUnitId = "ca-app-pub-6552333808021309/2189351430";
+#elif UNITY_ANDROID
+        string adUnitId = "ca-app-pub-6552333808021309/2189351430";      //orignal he
+#elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-3940256099942544/1712485313";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        this.rewardBasedVideo.LoadAd(this.CreateAdRequest(), adUnitId);
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public void ShowInterstitial()
+    {
+        if (this.interstitial.IsLoaded())
+        {
+            this.interstitial.Show();
+        }
+        else
+        {
+            MonoBehaviour.print("Interstitial is not ready yet");
+        }
+    }
+
+
+  
+
+  
+
+
+
+  public   bool isloaded = false;
+
+    public void ShowRewardBasedVideo()
+    {
+        if (this.rewardBasedVideo.IsLoaded())
+        {
+            isloaded = true;//try by me  to give rewards
+            this.rewardBasedVideo.Show();
+        }
+        else
+        {
+            MonoBehaviour.print("Reward based video ad is not ready yet");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #region Banner callback handlers
+
+    public void HandleAdLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLoaded event received");
+    }
+
+    public void HandleAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: " + args.Message);
+    }
+
+    public void HandleAdOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdOpened event received");
+    }
+
+    public void HandleAdClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdClosed event received");
+    }
+
+    public void HandleAdLeftApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLeftApplication event received");
+    }
+
+    #endregion
+
+    #region Interstitial callback handlers
+
+    public void HandleInterstitialLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleInterstitialLoaded event received");
+    }
+
+    public void HandleInterstitialFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print(
+            "HandleInterstitialFailedToLoad event received with message: " + args.Message);
+    }
+
+    public void HandleInterstitialOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleInterstitialOpened event received");
+    }
+
+    public void HandleInterstitialClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleInterstitialClosed event received");
+    }
+
+    public void HandleInterstitialLeftApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleInterstitialLeftApplication event received");
+    }
+
+    #endregion
+
+    #region RewardBasedVideo callback handlers
+
+    public void HandleRewardBasedVideoLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoLoaded event received");
+    }
+
+    public void HandleRewardBasedVideoFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print(
+            "HandleRewardBasedVideoFailedToLoad event received with message: " + args.Message);
+    }
+
+    public void HandleRewardBasedVideoOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoOpened event received");
+    }
+
+    public void HandleRewardBasedVideoStarted(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoStarted event received");
+    }
+
+    public void HandleRewardBasedVideoClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoClosed event received");
+    }
+
+    public void HandleRewardBasedVideoRewarded(object sender, Reward args)
+    {
+        giveReward = true;
+
+
+    }
+           
+
+    public void HandleRewardBasedVideoLeftApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoLeftApplication event received");
+    }
+    
+
+
+
+    #endregion
+}
