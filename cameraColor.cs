2b639476d172da9d7b91a270c494ec89c165ceec
smartyGirlz <35099078+smartyGirlz@users.noreply.github.com>
2019-01-05 21:28:18 +0530

    fade camera color transitions
    change camera bg color ,color transitns

---
+++ cameraColor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraColor : MonoBehaviour {
+
+    // Use this for initialization
+
+    Camera cam;
+
+    public float duration = 3.0F;
+
+    public Color c1, c2;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+
+        cam.clearFlags = CameraClearFlags.SolidColor;
+
+    }
+
+
+        // Update is called once per frame
+        void Update() {
+
+        float t = Mathf.PingPong(Time.time, duration) / duration;
+        cam.backgroundColor = Color.Lerp(c1, c2, t);
+      
+   
+
+    }
+    } 
