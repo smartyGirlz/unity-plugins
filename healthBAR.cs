2461d7c73ea6153e798a233d567020d686ae43b1
smartyGirlz <35099078+smartyGirlz@users.noreply.github.com>
2019-01-05 21:56:00 +0530

    easy impelementation
    health bar implementation

---
+++ healthBAR.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthBAR : MonoBehaviour {
+
+
+
+    //MOST EASY  WAY FOR HEALTH BAR
+
+
+
+    // Use this for initialization
+    Image healthbar;
+
+
+
+
+	void Start () {
+        healthbar = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        healthbar.fillAmount =   health.healthLeft/50f;
+		
+	}
+}
