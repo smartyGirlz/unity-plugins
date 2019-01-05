fc7449116b074d7562806b7f414a863ed0282be9
smartyGirlz <35099078+smartyGirlz@users.noreply.github.com>
2019-01-05 21:20:23 +0530

    background auto scale 2d gtame
    attach to bg ..any 2d bg  auto scale for diff screen size

---
+++ BGautoScale.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bgScaleHeadache : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        resize();
+	}
+	
+	// Update is called once per frame
+	void resize () {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+        float worldScreenHeight = Camera.main.orthographicSize * 2;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        transform.localScale = new Vector3(
+            worldScreenWidth / sr.sprite.bounds.size.x,
+            worldScreenHeight / sr.sprite.bounds.size.y, 1);
+    }
+}
