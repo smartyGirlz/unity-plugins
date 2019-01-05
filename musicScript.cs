83da0ad37ac91bf897a48887defd9b58ca322360
smartyGirlz <35099078+smartyGirlz@users.noreply.github.com>
2019-01-05 21:46:05 +0530

    no music repetions on same scene reloads
    common issue on same scene load/ button click
    though loop plays cont, another instance causes double  repetions

---
+++ musicScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicScript : MonoBehaviour {
+
+    // Use this for initialization
+
+    static Object instance = null;
+    void Start () {
+        //to continually play music in bg 
+        GameObject.DontDestroyOnLoad(gameObject);
+	}
+
+
+
+
+    
+
+   
+
+    void Awake()//no repeation of music k lie itta kuch
+    {
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    
+    }
+
