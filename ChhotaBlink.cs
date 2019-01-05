af11e09e5d6ea8570d1a1352a4c9de0fb45819fb
smartyGirlz <35099078+smartyGirlz@users.noreply.github.com>
2019-01-05 21:50:28 +0530

    Create ChhotaBlink.cs
    induce blinking effect any gameobject mini plugin

---
+++ ChhotaBlink.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChhotaBlink : MonoBehaviour
+{
+
+    public float reparrate;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        InvokeRepeating("Blink", 0, reparrate);
+    }
+
+    void Blink()
+    {
+        if (this.gameObject.activeSelf)
+        {
+            this.gameObject.SetActive(false);
+        }
+
+        else
+        {
+            this.gameObject.SetActive(true);
+        }
+
+    }
+}
