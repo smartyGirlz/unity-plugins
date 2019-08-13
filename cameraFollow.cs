284b9524bab6a878f67056937c8c65e89062df0f
surbhi shukla <surbhishukla38@gmail.com>
2019-08-13 18:24:56 +0530

    Create cameraFollow.cs
    camera follow + offset  for 3dfg

---
+++ cameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+
+
+    // A MUCH SIMPLER WAY OF DOING IS THIS IS MAKING THE CAMERA CHILD OF PLAYER
+
+     Transform target;
+  [SerializeField]  private Vector3 offset;
+
+
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+  
+       void LateUpdate()
+    {
+        FollowPlayer();
+    }
+
+    public void FollowPlayer()
+    {
+        transform.position = target.TransformPoint(offset);
+        transform.rotation = target.rotation;//so that camera also rotate with gameobject
+    }
+}
