3385b315ef3e8b93bfe3d1db3cc2083dd7f86377
surbhi shukla <surbhishukla38@gmail.com>
2020-03-17 19:37:49 +0530

    mobile local notification manager unity

---
+++ notificationManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Notifications.Android;
+
+public class notificationManager : MonoBehaviour
+{
+
+    private void Start()
+    {
+        createNotificationnChannelll();
+        sendNotification();
+    }
+
+    void createNotificationnChannelll()
+    {
+        var c = new AndroidNotificationChannel()
+        {
+            Id = "notif1",
+            Name = "Default Channel",
+            Importance = Importance.High,
+            Description = "Generic notifications",
+        };
+        AndroidNotificationCenter.RegisterNotificationChannel(c);
+    }
+  
+
+      void sendNotification()
+    {
+        var notification = new AndroidNotification();
+        notification.Title = "Goddess is in BattleGround";
+        notification.Text = "Play Now...";
+        // notification.FireTime = System.DateTime.Now.AddHours(24);
+         notification.FireTime = System.DateTime.Now.AddSeconds(5);
+
+        AndroidNotificationCenter.SendNotification(notification, "notif1");
+    }
+    //////now ALL YOU NEED TO IS ADD ICONS AND SCHEDULE IT FOR 6HRS OR MAYBE 12....THESE ARE REPEATIVE NOTIFICATIONS...
+    /////THAT WILL BE SCHEDULED ON START OF THIS.
+
+
+    //https://www.youtube.com/watch?v=qhC6OZPgd5Q
+
+}
