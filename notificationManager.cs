8c6122070f881e36ae7a2aea0b8bce9df1e2b119
surbhi shukla <surbhishukla38@gmail.com>
2020-04-24 18:13:00 +0530

    Update notificationManager.cs
    unity local notif

--- notificationManager.cs
+++ notificationManager.cs
@@ -23,22 +23,42 @@ public class notificationManager : MonoBehaviour
         };
         AndroidNotificationCenter.RegisterNotificationChannel(c);
     }
-  
 
+ 
       void sendNotification()
     {
         var notification = new AndroidNotification();
         notification.Title = "Goddess is in BattleGround";
         notification.Text = "Play Now...";
-        // notification.FireTime = System.DateTime.Now.AddHours(24);
-         notification.FireTime = System.DateTime.Now.AddSeconds(5);
+         notification.FireTime = System.DateTime.Now.AddHours(1);
+       
 
-        AndroidNotificationCenter.SendNotification(notification, "notif1");
+        var identifier= AndroidNotificationCenter.SendNotification(notification, "notif1");
+
+
+
+        //------------call back
+
+        if ( AndroidNotificationCenter.CheckScheduledNotificationStatus(identifier) == NotificationStatus.Scheduled)
+{
+    // Replace the currently scheduled notification with a new notification.
+    AndroidNotificationCenter.UpdateScheduledNotification(identifier, notification, "notif1");
+}
+else if ( AndroidNotificationCenter.CheckScheduledNotificationStatus(identifier) == NotificationStatus.Delivered)
+{
+    //Remove the notification from the status bar
+    AndroidNotificationCenter.CancelNotification(identifier);
+}
+else if ( AndroidNotificationCenter.CheckScheduledNotificationStatus(identifier) == NotificationStatus.Unknown)
+{
+    AndroidNotificationCenter.SendNotification(notification, "notif1");
+}
+        
     }
     //////now ALL YOU NEED TO IS ADD ICONS AND SCHEDULE IT FOR 6HRS OR MAYBE 12....THESE ARE REPEATIVE NOTIFICATIONS...
     /////THAT WILL BE SCHEDULED ON START OF THIS.
 
-
+   
     //https://www.youtube.com/watch?v=qhC6OZPgd5Q
 
 }
