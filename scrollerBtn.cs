30147c2eb0730d33120f830b8107953194ebb247
surbhi shukla <surbhishukla38@gmail.com>
2020-04-27 14:58:43 +0530

    Create scrollerBtn.cs
    Btn to Scroll ScrollrectElements

---
+++ scrollerBtn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class scrollerBtn : MonoBehaviour
+{
+    public void MoveContentPane(float value)  //attach method to scroll btn...>
+    {
+        var pos = transform.position;
+        pos.x += value;
+        transform.position = pos;
+    }
+
+    
+
+    public void reloadscene()
+    {
+        SceneManager.LoadScene("selectLevel");
+    }
+
+
+    public void loadFirstScreen()
+    {
+        SceneManager.LoadScene("MainScreen");
+    }
+}
